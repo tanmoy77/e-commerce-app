fee00a291030d35a3941f246f6b3940ed0ccc7ab
Tanmoy Joy <dukejay97@gmail.com>
2022-10-02 16:46:19 +0600

    added requirements.txt

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=5(9w3h38!4&g%wq_2ui_4ro6b(ee&49agay2_o*%no+%s@m&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
